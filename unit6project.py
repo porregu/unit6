e7e64ebd582d3ee79ee065133ebe4c6bb1c1d2b9
guillermo <porregu@ssfs.org>
2019-11-06 09:44:04 -0500

    unit 6 finish

--- unit6project.py
+++ unit6project.py
@@ -1,13 +1,30 @@
-number=int(input("until what number you want ?"))
-list=[]
-for x in range(2, number+1):
-        list.append(x)
-primes=[]
-while len(list)>0:
-    track=list[0]
-    primes.append(list[0])
-    for x in list:
-        if x%track==0:
-            list.remove(x)
+def get_list():
+    '''
 
-print("this is the primes numbers until ",number, primes)
+    :return:this get a list of number, only the user need to type the higest number
+    '''
+    number=int(input("what you want to be your maximun number?"))# this line ask the user for a number to be the higest number
+    list=[]# this creat a blank list to put the numbers in
+    for x in range(2, number+1):# this loop make to put the first number in the blank list
+            list.append(x)
+    return list
+
+def get_primes(list):
+    '''
+
+    :param list:this function show the primes of the list give above by the user
+    :return:
+    '''
+    primes=[]# this creat a blank list to put the numbers in
+    while len(list)>0:# this shows all the numbers in the list until the higest number put by the user
+        track=list[0]# this is blank list to keep track of numbers
+        primes.append(list[0])# this take the list numners and added to primes
+        for x in list:# this is a loop for reapeating the proces until is done
+            if x%track==0:# this will add all the numbers that divided by "track" are not equal to 0
+                list.remove(x)# this will remove all the numbers that divided by track equal 0
+    print("this is your prime numbers", primes)
+def main():
+    list=get_list()
+    get_primes(list)
+
+main()
